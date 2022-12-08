c3835dccd04f51bc2c2e20379e4af343076c13e1
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-12-08 10:43:28 +0300

    Clean up code

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./normalize.css";
 import App from "./Components/App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,10 +11,3 @@ root.render(
     <App tab="home"/>
   </BrowserRouter>
 );
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
