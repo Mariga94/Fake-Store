2a399332bca66c2b4b6517c488816a24a28c222c
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-12-08 05:38:36 +0300

    Revert to Browser router

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App tab="home"/>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // ReactDOM.render(
